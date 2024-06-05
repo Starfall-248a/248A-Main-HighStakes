7f0c9225c52dc136fdf7592570e9e036eadb2282
248A_Starfall <Starfall-248a@users.noreply.github.com>
2024-06-04 23:12:21 -0400

    Camp updates

--- include/Customs/robot.hpp
+++ include/Customs/robot.hpp
@@ -1,10 +1,11 @@
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
 #include "pros/adi.hpp"
+#include "pros/distance.hpp"
 #include "pros/imu.hpp"
 #include "pros/motor_group.hpp"
 #include "pros/rotation.hpp"
-#include "lemlib/chassis/chassis.hpp"
-#include "lemlib/chassis/trackingWheel.hpp"
-#include "pros/distance.hpp"
+
 
 using namespace pros;
 using namespace lemlib;
@@ -13,27 +14,14 @@ extern Controller controller;
 
 extern MotorGroup driveLeft;
 extern MotorGroup driveRight;
-extern MotorGroup Lift;
 
-extern Motor liftRight;
-extern Motor liftLeft;
-extern Motor intakeTop;
-extern Motor intakeBottom;
-
-extern MotorGroup Intake;
+extern Motor Intake;
+extern Motor Clamp;
 
 extern Imu imu;
 extern Distance distance;
-
-extern adi::DigitalOut Hang;
-extern adi::DigitalOut PTO;
-
-extern Rotation latRot;
-extern Rotation angRot;
+extern Rotation clampRot;
 
 extern Drivetrain drivetrain;
-
-extern TrackingWheel horizontal_tracking_wheel;
-extern TrackingWheel vertical_tracking_wheel;
 
 extern Chassis chassis;
--- src/Headers/robot.cpp
+++ src/Headers/robot.cpp
@@ -3,7 +3,6 @@
 #include "pros/adi.h"
 #include "pros/adi.hpp"
 #include "pros/misc.h"
-#include "pros/motors.h"
 #include "pros/motors.hpp"
 
 using namespace pros;
@@ -11,19 +10,15 @@ using namespace lemlib;
 
 Controller controller(E_CONTROLLER_MASTER);
 
-MotorGroup driveLeft({-13, -12, -11}, pros::MotorGearset::blue); // left motors on ports 13, 12, 11
-MotorGroup driveRight({20, 19, 18}, pros::MotorGearset::blue); // right motors on ports 20, 19, 18
-MotorGroup Lift({});
+MotorGroup driveLeft({-13, -12}, pros::MotorGearset::blue); // left motors on ports 13, 12, 11
+MotorGroup driveRight({20, 19}, pros::MotorGearset::blue); // right motors on ports 20, 19, 18
 
-Motor liftLeft(-11);
-Motor liftRight(18);
-Motor intakeTop(4);
-Motor intakeBottom(5);
-
-MotorGroup Intake({14, -17}, pros::MotorGearset::blue); //delete later
+Motor Intake(4);
+Motor Clamp(5);
 
 Imu imu(1);
 Distance distance(3);
+Rotation clampRot(6);
 
 Rotation latRot(7);
 Rotation angRot(8);
@@ -36,18 +31,13 @@ Drivetrain drivetrain(&driveLeft, // left motor group
                               &driveRight, // right motor group
                               12.482, // 10 inch track width
                               lemlib::Omniwheel::NEW_4, // using new 4" omnis
-                              343, // drivetrain rpm is 360
+                              257, // drivetrain rpm is 360
                               2 // horizontal drift is 2 (for now)
 );
 
-// horizontal tracking wheel
-TrackingWheel horizontal_tracking_wheel(&angRot, lemlib::Omniwheel::NEW_275, -5.75);
-// vertical tracking wheel
-TrackingWheel vertical_tracking_wheel(&latRot, lemlib::Omniwheel::NEW_275, -2.5);
-
-OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
                             nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
-                            &horizontal_tracking_wheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 1
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             &imu // inertial sensor
 );
