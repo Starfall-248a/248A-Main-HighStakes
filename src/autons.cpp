9e15719062bfe5e52a7e0bac09a0348068c1b703
248A_Starfall <58570550+Starfall-248a@users.noreply.github.com>
2024-10-07 08:35:03 -0400

    Winpoint auton basis

--- src/autons.cpp
+++ src/autons.cpp
@@ -1,3 +1,4 @@
+#include "lemlib/chassis/chassis.hpp"
 #include "main.h"
 #include "pros/adi.h"
 
@@ -14,9 +15,22 @@ void blueSoloWP(){
     Clamp.set_value(HIGH);
     intake.move(127);
     chassis.moveToPoint(23.542, 46.871, 1100);
-    chassis.waitUntilDone();
     chassis.moveToPose(12, 12, 180, 2000);
+    chassis.waitUntilDone();
     intake.brake();
+    Clamp.set_value(LOW);
+    chassis.moveToPoint(10.5, -16, 1500);
+    chassis.turnToHeading(300, 750);
+    chassis.moveToPoint(16.5, -19.5, 500, {.forwards = false});
+    chassis.waitUntilDone();
+    Clamp.set_value(HIGH);
+    intake.move(127);
+    chassis.moveToPoint(23.5, -47, 1500);
+    chassis.moveToPoint(24,-10, 2000);
+    chassis.waitUntilDone();
+    Clamp.set_value(LOW);
+    intake.brake();
+    chassis.moveToPoint(24, -6, 2000, {.maxSpeed = 50});
 }
 
 void fourRingRed(){
